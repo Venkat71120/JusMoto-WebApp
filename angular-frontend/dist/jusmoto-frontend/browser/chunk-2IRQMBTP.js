b2d3146b6c7a45de7c8401bf4f57e5b7e1b76d32
sharanchandru-BITS <sharan@blackitechs.com>
2026-02-24 16:03:15 +0530

    added order checkout with cod

---
+++ angular-frontend/dist/jusmoto-frontend/browser/chunk-2IRQMBTP.js
@@ -0,0 +1,1 @@
+import"./chunk-TMC7WMLO.js";var n=[{path:"",loadComponent:()=>import("./chunk-XZNRRQLK.js").then(o=>o.FranchiseDashboardComponent)}];export{n as franchiseRoutes};
--- angular-frontend/dist/jusmoto-frontend/browser/chunk-YVKHZFNM.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-TMC7WMLO.js";var n=[{path:"",loadComponent:()=>import("./chunk-HVGSTHFN.js").then(o=>o.FranchiseDashboardComponent)}];export{n as franchiseRoutes};
